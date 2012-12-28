b2e83ce6050da22655cfe8a7ccb459c7bc6eb718
Brian Matejek <bmatejek@gmail.com>
2012-12-28 00:18:53 -0500

    Brian's simulation outside of the folder

--- user.py
+++ user.py
@@ -2,26 +2,26 @@ import random
 
 class User(object):
 
-	def __init__(self, user_type=0, num_stories=10):
-		self.user_type = user_type
-		self.voting_history = [0]*10
+	def __init__(self, user_bias):
+		self.user_bias = user_bias
+		self.voting_history = [] * 10000
 
 	def vote(self, story_type, story_index):
 		voting_percentage = .9
 		voting_bias = .9 #Chance they will vote for their side, for cliques only
 		self.voting_history[story_index] = 0
 		if (random.random() < voting_percentage):
-			if (self.user_type == 0): #normal users
+			if (self.user_bias == 0): #normal users
 				if (random.randint(0, 1)):
 					self.voting_history[story_index] = 1
 				else:
 					self.voting_history[story_index] = -1
-			elif (self.user_type == story_type): #biased in favor
+			elif (self.user_bias == story_type): #biased in favor
 				if (random.random() < voting_bias):
 					self.voting_history[story_index] = 1
 				else:
 					self.voting_history[story_index] = -1
-			elif (self.user_type != story_type): #biased against
+			elif (self.user_bias != story_type): #biased against
 				if (random.random() > voting_bias):
 					self.voting_history[story_index] = 1
 				else:
